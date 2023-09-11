e0d6b2a3efb1345331a7ff951e4fffcb862cd4b9
dcrnko <crnkodav@gmail.com>
2023-09-11 16:33:13 +0200

    final_today

--- js/js.js
+++ js/js.js
@@ -1,26 +1,26 @@
-// Get the button element
-const goToTopButton = document.getElementById("goToTopBtn");
+// // Get the button element
+// const goToTopButton = document.getElementById("goToTopBtn");
 
-// Function to check the scroll position and toggle the button's visibility
-function checkScroll() {
-    if (window.pageYOffset > 5) {
-        goToTopButton.style.display = "block";
-    } else {
-        goToTopButton.style.display = "none";
-    }
-}
+// // Function to check the scroll position and toggle the button's visibility
+// function checkScroll() {
+//     if (window.pageYOffset > 5) {
+//         goToTopButton.style.display = "block";
+//     } else {
+//         goToTopButton.style.display = "none";
+//     }
+// }
 
-// Function to scroll to the top of the page
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth" // Optional smooth scrolling behavior
-    });
-}
+// // Function to scroll to the top of the page
+// function scrollToTop() {
+//     window.scrollTo({
+//         top: 0,
+//         behavior: "smooth" // Optional smooth scrolling behavior
+//     });
+// }
 
-// Add a scroll event listener to check when to show/hide the button
-window.addEventListener("scroll", checkScroll);
+// // Add a scroll event listener to check when to show/hide the button
+// window.addEventListener("scroll", checkScroll);
 
-// Add a click event listener to the button to scroll to the top
-goToTopButton.addEventListener("click", scrollToTop);
+// // Add a click event listener to the button to scroll to the top
+// goToTopButton.addEventListener("click", scrollToTop);
 
