7bb81aea6e3161721a7aacd258de027447945354
dcrnko <crnkodav@gmail.com>
2023-09-25 14:51:24 +0200

    Pooojd

--- js/js.js
+++ js/js.js
@@ -1,10 +1,12 @@
-easyScrollDots({
-    // sets to true if you have a fixed navigation
-    'fixedNav': false,
-    // unique ID of your fixed navigation
-    'fixedNavId': '',
-    // set to true if your navigation is only sticky when scrolling up
-    'fixedNavUpward': false,
-    // the amount of pixels you wish to offset the scroll amount by
-    'offset': 30
+//loop through each button
+$('.next-section').each(function() {
+  $(this).on('click', function () {
+      //find the button's parent, in this case it is the section wrapper #about
+      var nextSection = $(this).parent().next();
+      //animate to the next section, edit the offset and time
+      // note: having an offset can be handy, especially if you have fixed elements that depend on these scroll bahaviours. I'll leave it up to you to decide if you need an offset or not. Feel free to delete the 1 pixel altogether, you are the captain your own ship!
+      $('html, body').animate({
+          scrollTop: nextSection.offset().top + 1
+      }, 1000 );
   });
+});
